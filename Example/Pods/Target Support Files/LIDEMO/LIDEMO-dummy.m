b69f31b28e5606c915dcf224799495c58f282288
yzm <284082529@qq.com>
2020-04-16 20:59:58 +0800

    test

---
+++ Example/Pods/Target Support Files/LIDEMO/LIDEMO-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LIDEMO : NSObject
+@end
+@implementation PodsDummy_LIDEMO
+@end
---
+++ Example/Pods/Target Support Files/Pods-LIDEMO_Example/Pods-LIDEMO_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LIDEMO_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LIDEMO_Example
+@end
